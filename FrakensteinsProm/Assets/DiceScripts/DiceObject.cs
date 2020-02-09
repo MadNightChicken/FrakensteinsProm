469219ce87d62ce88a2b9b577bab559e09174634
Joe Ross <joe.ross@charter.net>
2020-02-09 15:05:29 -0600

    just adding more things that are being anoying

---
+++ FrakensteinsProm/Assets/DiceScripts/DiceObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Sprite[] diceSides;
+    private SpriteRenderer spriteRenderer;
+    public Image imageIcon;
+    public int value;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+        RollTheDice();
+    }
+
+    public void RollTheDice()
+    {
+        var diceRolled = Random.Range(0, 6);
+        value = diceRolled + 1;
+        imageIcon.sprite = diceSides[diceRolled];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FrakensteinsProm/Assets/DiceScripts/DragHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Transform startParent;
+    Transform defaultparent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        defaultparent = startParent.parent.parent.parent.transform;
+        transform.SetParent(defaultparent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent || transform.parent == defaultparent)
+        {
+            transform.position = startPosition;
+            transform.parent = startParent;
+        }
+    }
+}
