7cbe47c52da8e55565a309fc597e685a49e0094f
Joe Ross <joe.ross@charter.net>
2020-02-23 23:06:55 -0600

    adding new dice scripts

---
+++ FrakensteinsProm/Assets/DiceScripts/DiceSlot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DiceSlot : MonoBehaviour, IDropHandler
+{
+    public GameObject dice
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("dragged");
+        //if (!dice)
+        //{
+            DragHandler.itemBeingDragged.transform.SetParent(transform);
+            //do stuff
+        //}
+    }
+}
